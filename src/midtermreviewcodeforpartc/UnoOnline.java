eb5e3d46bc0bab8e7cbf1d9dd976e2d9f6939007
hongmei Zhang <zhahongm@sheridanc.on.ca>
2022-03-12 01:03:15 -0500

    new validation class

--- src/midtermreviewcodeforpartc/UnoOnline.java
+++ src/midtermreviewcodeforpartc/UnoOnline.java
@@ -1,19 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package midtermreviewcodeforpartc;
 
 import java.util.Scanner;
 
-/**
- * The signIn page for UnoOnline.
- * Takes in a user name and password and then
- * validates the password before creating a new user in the list
- * of possible users. To be used as the beginning code for MidtermReview C.
- * @author dancye, 2019
- */
+
 public class UnoOnline 
 {
     private User users[] = new User[100];//room for 100 online players!
